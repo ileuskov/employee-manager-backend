86d9ede55079ddc5a2ee31c0aea72e20b77df9a7
Ivan Leuskov <ileuskov@gmail.com>
2022-10-20 18:15:11 +0200

    create service layer

---
+++ src/main/java/com/ileuskov/employeeManager/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package com.ileuskov.employeeManager.service;
+
+import com.ileuskov.employeeManager.exception.EmployeeNotFoundException;
+import com.ileuskov.employeeManager.model.Employee;
+import com.ileuskov.employeeManager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.UUID;
+
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    //adding employee using employee Repo
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    // implement optional interface by throwing an exception if no Employee is found
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee by id" + id + "was not found"));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
