5aaad4dc7c430fc82ee102c458427fb6717473a0
Ivan Leuskov <ileuskov@gmail.com>
2022-10-20 16:10:40 +0200

    create backend repo for the app

---
+++ src/main/java/com/ileuskov/employeeManager/EmployeeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ileuskov.employeeManager;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagerApplication.class, args);
+	}
+
+}
