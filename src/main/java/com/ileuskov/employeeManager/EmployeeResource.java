a82a12fa13caf28123b066918acf8e1595bd1f97
Ivan Leuskov <ileuskov@gmail.com>
2022-10-20 19:51:12 +0200

    fix: missing @service annotation in employee service

---
+++ src/main/java/com/ileuskov/employeeManager/EmployeeResource.java
@@ -0,0 +1,2 @@
+package com.ileuskov.employeeManager;public class EmployeeResource {
+}
--- src/main/java/com/ileuskov/employeeManager/service/EmployeeService.java
+++ src/main/java/com/ileuskov/employeeManager/service/EmployeeService.java
@@ -4,10 +4,12 @@ import com.ileuskov.employeeManager.exception.EmployeeNotFoundException;
 import com.ileuskov.employeeManager.model.Employee;
 import com.ileuskov.employeeManager.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class EmployeeService {
     private final EmployeeRepo employeeRepo;
 
